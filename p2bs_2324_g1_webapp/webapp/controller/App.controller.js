4cfa5c51dbc8840e8f3d7542e8cdebe3985e0b03
Robbe Van Duysen <robbe.vanduysen@student.hogent.be>
2024-03-13 13:23:26 +0100

    Setup and testing SAPUI5 functionalities

---
+++ p2bs_2324_g1_webapp/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("p2bs2324g1webapp.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
