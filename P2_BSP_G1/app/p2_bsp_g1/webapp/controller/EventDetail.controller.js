148bd52f04e2ed0aa3c8083fb6ab8d21673bca2f
Robbe Van Duysen <robbe.vanduysen@student.hogent.be>
2024-03-29 22:41:14 +0100

    Toevoegen info detailEvent page

--- P2_BSP_G1/app/p2_bsp_g1/webapp/controller/EventDetail.controller.js
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/EventDetail.controller.js
@@ -1,15 +1,27 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast) {
+    "use strict";
 
-        return Controller.extend("p2bspg1.controller.EventDetail", {
-            onInit: function () {
+    return Controller.extend("p2bspg1.controller.EventDetail", {
+      onInit: function () {
+        this.oOwnerComponent = this.getOwnerComponent();
+        this.oRouter = this.oOwnerComponent.getRouter();
+        this.oRouter.attachRouteMatched(this._onRouteMatched, this);
+      },
+      signUp: function (evt) {
+        MessageToast.show("U bent succesvol ingeschreven voor dit evenement!");
+      },
+      _onRouteMatched: function (oEvent) {
+        var oArgs = oEvent.getParameter("arguments");
+        var oView = this.getView();
+        var urlPath = "/" + oArgs.evenementID;
 
-            }
-        });
+        oView.bindElement({ path: urlPath });
+      },
     });
+  }
+);
