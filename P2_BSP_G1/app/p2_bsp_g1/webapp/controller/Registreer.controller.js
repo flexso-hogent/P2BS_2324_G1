941c044a123845e4195ec7e53a8dc5cf669a5148
Taylon Vandenbroecke <taylon.vandenbroecke@student.hogent.be>
2024-04-13 12:57:41 +0200

    tried to add registration form

---
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/Registreer.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, MessageToast) {
+      "use strict";
+  
+      return Controller.extend("p2bspg1.controller.Registreer", {
+        onInit: function () {
+        }
+    })
+});
