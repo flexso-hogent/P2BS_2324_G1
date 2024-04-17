08e7511f8d57b735fed6f97d579ca32b41a344eb
Taylon Vandenbroecke <taylon.vandenbroecke@student.hogent.be>
2024-04-17 13:15:29 +0200

    logica toegevoegd aan registratiepagina
    niet alle velden ingevuld of geen matchend passwoord

--- P2_BSP_G1/app/p2_bsp_g1/webapp/controller/Registreer.controller.js
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/Registreer.controller.js
@@ -1,91 +1,85 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap/m/MessageBox",],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller, JSONModel, MessageBox) {
-      "use strict";
-  
-      return Controller.extend("p2bspg1.controller.Registreer", {
-        onInit: function () {
-          var oRegistreer = {
-            achternaam: "",
-            email: "",
-            geboortedatum: null,
-            // gebruikerID: ,
-            voornaam: "",
-            wachtwoord: "",
-          };
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap/m/MessageBox"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel, MessageBox) {
+    "use strict";
 
-          var oModel = new JSONModel(oRegistreer);
-          this.getView().setModel(oModel, "form");
-        },
-      
-        onRegister: function() {
-          var oForm = this.getView().getModel("form").getData();
-          // oForm.geboortedatum = new Date(oForm.geboortedatum);
+    return Controller.extend("p2bspg1.controller.Registreer", {
+      onInit: function () {
+        var oRegistreer = {
+          achternaam: "",
+          email: "",
+          geboortedatum: null,
+          voornaam: "",
+          wachtwoord: ""
+        };
 
-          var odatamodel = this.getView().getModel("v2model");
-
-
-          console.log(oForm);
-
-
-          odatamodel.create("/Gebruikers", oForm, {
-            success: function (data, response) {
-              console.log("gelukt");
-              MessageBox.success("Data was created successfully");
-            },
-            error: function (error) {
-              console.log("niet gelukt");
-              MessageBox.error("Error while creating the data");
-            },
-          });
-          console.log("done");
-
-          // Validation
-          // if (!data.voornaam || !data.email || !data.password || !data.confirmPassword) {
-          //   MessageToast.show("Please fill in all fields.");
-          //   return;
-          // }
+        var oModel = new JSONModel(oRegistreer);
+        this.getView().setModel(oModel, "form");
+      },
     
-          // if (data.password !== data.confirmPassword) {
-          //   MessageToast.show("Passwords do not match.");
-          //   return;
-          // }
-    
-          // Perform registration
-          // Here you can call your backend API to register the user
-          // Example:
-          // jQuery.ajax({
-          //   url: "/register",
-          //   method: "POST",
-          //   data: data,
-          //   success: function(response) {
-          //     MessageToast.show("Registration successful!");
-          //   },
-          //   error: function(error) {
-          //     MessageToast.show("Registration failed. Please try again later.");
-          //   }
-          // });
-        },
-        onSeePassword: function() {
-          var oPasswordInput = this.getView().byId("wachtwoord");
-          var sCurrentType = oPasswordInput.getType();
-          if (sCurrentType === "Password") {
-              oPasswordInput.setType("Text");
-          } else {
-              oPasswordInput.setType("Password");
+      onRegister: function() {
+        var oForm = this.getView().getModel("form").getData();
+        var sHerhaalWachtwoord = this.getView().byId("herhaalWachtwoord").getValue();
+
+        // Check if any field is empty
+        if (!this.validateForm(oForm)) {
+          MessageBox.error("Please fill in all fields");
+          return;
+        }
+
+        // Check if password and repeat password match
+        if (oForm.wachtwoord !== sHerhaalWachtwoord) {
+          MessageBox.error("Passwords do not match");
+          return;
+        }
+
+        var odatamodel = this.getView().getModel("v2model");
+
+        odatamodel.create("/Gebruikers", oForm, {
+          success: function (data, response) {
+            console.log("gelukt");
+            MessageBox.success("Uw account is aangemaakt!");
+          },
+          error: function (error) {
+            console.log("niet gelukt");
+            MessageBox.error("Het is niet gelukt om uw account aan te maken, probeer opnieuw!");
+          },
+        });
+        console.log("done");
+      },
+
+      // Function to validate form fields
+      validateForm: function(formData) {
+        for (var key in formData) {
+          if (formData.hasOwnProperty(key)) {
+            if (!formData[key] && key !== "herhaalWachtwoord") {
+              return false;
+            }
           }
-        },
-        onSeePasswordHerhaal: function() {
-          var oPasswordInput = this.getView().byId("herhaalWachtwoord");
-          var sCurrentType = oPasswordInput.getType();
-          if (sCurrentType === "Password") {
-              oPasswordInput.setType("Text");
-          } else {
-              oPasswordInput.setType("Password");
-          }
+        }
+        return true;
+      },
+
+      onSeePassword: function() {
+        var oPasswordInput = this.getView().byId("wachtwoord");
+        var sCurrentType = oPasswordInput.getType();
+        if (sCurrentType === "Password") {
+            oPasswordInput.setType("Text");
+        } else {
+            oPasswordInput.setType("Password");
+        }
+      },
+      onSeePasswordHerhaal: function() {
+        var oPasswordInput = this.getView().byId("herhaalWachtwoord");
+        var sCurrentType = oPasswordInput.getType();
+        if (sCurrentType === "Password") {
+            oPasswordInput.setType("Text");
+        } else {
+            oPasswordInput.setType("Password");
+        }
       }  
-      });
     });
+  });
