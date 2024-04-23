e00a3f69f8225ac6f4ff39afee1247769a9f9f4c
Robbe Van Duysen <robbe.vanduysen@student.hogent.be>
2024-04-23 10:16:59 +0200

    Create CreateSessie

---
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/CreateSessie.controller.js
@@ -0,0 +1,58 @@
+sap.ui.define(
+  [
+    "sap/ui/core/library",
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (coreLibrary, Controller, JSONModel) {
+    "use strict";
+
+    var ValueState = coreLibrary.ValueState;
+
+    return Controller.extend("p2bspg1.controller.CreateSessie", {
+      onInit: function () {
+      },
+      handleLiveChange: function (oEvent) {
+        var oTextArea = oEvent.getSource(),
+          iValueLength = oTextArea.getValue().length,
+          iMaxLength = oTextArea.getMaxLength(),
+          sState =
+            iValueLength > iMaxLength ? ValueState.Warning : ValueState.None;
+
+        oTextArea.setValueState(sState);
+      },
+
+      handleSimpleExceeding: function (oEvent) {
+        var oTA = oEvent.getSource();
+        oEvent.getParameter("exceeded")
+          ? oTA.setValueState(ValueState.Error)
+          : oTA.setValueState(ValueState.Success);
+      },
+
+      buttonSetShortValuePress: function () {
+        this.byId("textAreaWithBinding2").setValue("Small Text");
+        this.byId("textAreaWithoutBinding").setValue("Small Text");
+      },
+
+      buttonSetLongValuePress: function () {
+        var sText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+        this.byId("textAreaWithBinding").setValue(sText);
+        this.byId("textAreaWithBinding2").setValue(sText);
+      },
+
+      buttonToggleShowExceededTextPress: function () {
+        var oTA = this.byId("textAreaWithBinding2");
+        oTA.setShowExceededText(!oTA.getShowExceededText());
+        var oTA = this.byId("textAreaWithoutBinding");
+        oTA.setShowExceededText(!oTA.getShowExceededText());
+      },
+      annuleer() {
+        history.back();
+      },
+
+      createSessie() {
+        history.back();
+      },
+    });
+  }
+);
