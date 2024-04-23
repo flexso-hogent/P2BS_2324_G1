612230c172744fb19ad74a7e27a69e39b7db72b5
Robbe Van Duysen <robbe.vanduysen@student.hogent.be>
2024-04-23 09:52:46 +0200

    Update CreateEvent.controller.js, style.css, and CreateEvent.view.xml

--- P2_BSP_G1/app/p2_bsp_g1/webapp/controller/CreateEvent.controller.js
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/CreateEvent.controller.js
@@ -1,19 +1,20 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-        return Controller.extend("p2bspg1.controller.App", {
-            onInit: function () {
-                // Initialization code if needed
-            },
-            onCancel: function() {
-                window.location.href ="#/StartScreen/"; // Redirect to home page
-            },
-        });
-        
+    return Controller.extend("p2bspg1.controller.App", {
+      onInit: function () {},
+      annuleer() {
+        history.back();
+      },
+
+      createEvent() {
+        this.getOwnerComponent().getRouter().navTo("Events");
+      },
     });
+  }
+);
