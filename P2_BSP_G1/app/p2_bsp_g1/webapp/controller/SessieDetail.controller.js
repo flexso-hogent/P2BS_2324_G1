5938f31c09e41bdb315470ae997930c5eebbddc1
Robbe Van Duysen <robbe.vanduysen@student.hogent.be>
2024-04-25 22:03:00 +0200

    Adding remove sessie

--- P2_BSP_G1/app/p2_bsp_g1/webapp/controller/SessieDetail.controller.js
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/SessieDetail.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/m/MessageBox"],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageToast) {
+  function (Controller, MessageToast, MessageBox) {
     "use strict";
+    var sessieID = 0;
 
     return Controller.extend("p2bspg1.controller.SessieDetail", {
       onInit: function () {
@@ -13,13 +14,47 @@ sap.ui.define(
         this.oRouter
           .getRoute("SessieDetail")
           .attachPatternMatched(this._onRouteMatched, this);
+
+        var oAv3 = this.byId("av3"),
+          oAv4 = this.byId("av4");
+        if (
+          !localStorage.getItem("user") === null ||
+          localStorage.getItem("user").includes('"rol":"admin"')
+        ) {
+          oAv3.setVisible(true);
+          oAv4.setVisible(true);
+        }
       },
       _onRouteMatched: function (oEvent) {
         var oArgs = oEvent.getParameter("arguments");
         var oView = this.getView();
         var urlPath = "/" + "Sessies(sessieID=" + oArgs.sessieID + ")";
+        sessieID = oArgs.sessieID;
 
         oView.bindElement({ path: urlPath });
+      },
+      editSessie: function () {
+        // window.location.href = "#Sessies#/edit/" + evenementID;
+      },
+      deleteSessie: function () {
+        var odatamodel = this.getView().getModel("v2model");
+
+        odatamodel.remove("/Sessies(" + sessieID + ")", {
+          success: function (data, response) {
+            console.log("gelukt");
+            MessageBox.success("De sessie is succesvol verwijderd!", {
+              onClose: function () {
+                history.back();
+              },
+            });
+          },
+          error: function (error) {
+            console.log("niet gelukt");
+            MessageBox.error(
+              "Het is niet gelukt om het evenement te verwijderen. Probeer het opnieuw!"
+            );
+          },
+        });
       },
     });
   }
