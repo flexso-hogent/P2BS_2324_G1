44f33c7efa1b5d1c24cda77346aa0b2a3af59010
Robbe Van Duysen <robbe.vanduysen@student.hogent.be>
2024-04-30 16:05:49 +0200

    Fixing issues with displaying user profile and console logs in Profiel.controller.js

--- P2_BSP_G1/app/p2_bsp_g1/webapp/controller/Feedback.controller.js
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/Feedback.controller.js
@@ -2,28 +2,28 @@ sap.ui.define(
   ["sap/ui/core/mvc/Controller", "sap/m/MessageBox"],
   function (Controller, MessageBox) {
     "use strict";
-    var user = localStorage.getItem("user"),
-      evenementID = 0;
+    var user = localStorage.getItem("user");
+
     return Controller.extend("p2bspg1.controller.Feedback", {
       onInit: function () {
         if (user == null) {
           this.getOwnerComponent().getRouter().navTo("NotFound");
         }
 
-        this.oOwnerComponent = this.getOwnerComponent();
-        this.oRouter = this.oOwnerComponent.getRouter();
-        this.oRouter
-          .getRoute("Feedback")
-          .attachPatternMatched(this._onRouteMatched, this);
+        // this.oOwnerComponent = this.getOwnerComponent();
+        // this.oRouter = this.oOwnerComponent.getRouter();
+        // this.oRouter
+        //   .getRoute("Feedback")
+        //   .attachPatternMatched(this._onRouteMatched, this);
       },
-      _onRouteMatched: function (oEvent) {
-        var oArgs = oEvent.getParameter("arguments");
-        evenementID = oArgs.evenementID;
-        var oView = this.getView();
-        var urlPath = "/" + "Score(evenementID=" + oArgs.evenementID + ")";
+      // _onRouteMatched: function (oEvent) {
+      //   var oArgs = oEvent.getParameter("arguments");
+      //   evenementID = oArgs.evenementID;
+      //   var oView = this.getView();
+      //   var urlPath = "/" + "Score(evenementID=" + oArgs.evenementID + ")";
 
-        oView.bindElement({ path: urlPath });
-      },
+      //   oView.bindElement({ path: urlPath });
+      // },
     });
   }
 );
