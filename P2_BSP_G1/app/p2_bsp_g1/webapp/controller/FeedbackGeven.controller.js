978803e18a0df34044d1ed780c688ef707e31be4
Taylon Vandenbroecke <taylon.vandenbroecke@student.hogent.be>
2024-05-06 11:47:49 +0200

    feedback geven works!

--- P2_BSP_G1/app/p2_bsp_g1/webapp/controller/FeedbackGeven.controller.js
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/FeedbackGeven.controller.js
@@ -6,17 +6,17 @@ sap.ui.define([
 ], function(Controller, JSONModel, Toast, MessageBox) {
 	"use strict";
 
-	return Controller.extend("your.namespace.FeedbackGeven", {
+  var user = JSON.parse(localStorage.getItem("user"));
+
+	return Controller.extend("p2bspg1.controller.FeedbackGeven", {
 
 		onInit: function() {
 			var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
 			this.getView().setModel(oModel);
-      if (
-        localStorage.getItem("user") == null ||
-        !localStorage.getItem("user").includes('"rol":"user"')
-      ) {
+      if (user == null) {
         this.getOwnerComponent().getRouter().navTo("NotFound");
       }
+      
 		},
 		handleChange: function(oEvent) {
 			var demoToast = this.getView().byId("demoToast");
@@ -41,13 +41,48 @@ sap.ui.define([
           MessageBox.error("Feedback is niet verzonden. Vul feedback in.");
           return;
         } else {
-          MessageBox.success("Feedback met " + stars + " sterren is verzonden.",
-            {
+          var odatamodel = this.getView().getModel("v2model");
+          var inschrijvingID = this.getInschrijvingsID();
+          console.log("inschrijvingID", inschrijvingID);
+          
+          var oForm = {
+            "gebruikerID_gebruikerID": user.gebruikerID, 
+            "inschrijvingID_inschrijvingID": inschrijvingID,
+            "feedback": feedback,
+            "aantalSterren": stars,
+          }
+
+          console.log("oForm", oForm);
+
+        odatamodel.create("/Scores", oForm, {
+          success: function (data, response) {
+            MessageBox.success("Feedback met " + stars + " sterren is verzonden.", {
               onClose: function () {
-                window.location.href = "#/";
+                history.back();
               },
-            }); 
+            });
+          },
+          error: function (error) {
+            MessageBox.error(
+              "Het is niet gelukt om uw feedback aan te maken, probeer opnieuw!"
+            );
+          },
+        });
+          // MessageBox.success("Feedback met " + stars + " sterren is verzonden.",
+          //   {
+          //     onClose: function () {
+          //       window.location.href = "#/";
+          //     },
+          //   }); 
         }
+    },
+
+    getInschrijvingsID: function() {
+      var oComponent = this.getOwnerComponent();
+          var oRouter = oComponent.getRouter();
+          var oArgs = oRouter.getHashChanger().getHash().split("/");
+          console.log(oArgs);
+          return parseInt(oArgs[oArgs.length - 1], 10);
     }
 	});
 });
