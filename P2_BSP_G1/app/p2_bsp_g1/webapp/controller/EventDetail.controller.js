11df38b223013ba9f8028f32f4da8501c418e706
Taylon Vandenbroecke <taylon.vandenbroecke@student.hogent.be>
2024-05-14 11:56:59 +0200

    fix(refresh): should not refresh if you open the editEvent now

--- P2_BSP_G1/app/p2_bsp_g1/webapp/controller/EventDetail.controller.js
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/EventDetail.controller.js
@@ -265,15 +265,19 @@ sap.ui.define(
         });
       },
       onEventPressEdit: function (EvenementID) {
+        var oRouter = this.getOwnerComponent().getRouter();
         var sHash = window.location.hash;
-
+    
         var aHashParts = sHash.split("/");
         var sEventId = aHashParts[aHashParts.length - 1];
         console.log(sEventId);
-        this.getOwnerComponent().getRouter().navTo("EditEvent", {
-          EvenementID: sEventId,
-        });
+    
+        // Navigate to "EditEvent" route without full page reload
+        oRouter.navTo("EditEvent", {
+            EvenementID: sEventId
+        }, /* bReplace */ true);
       },
+    
       onTerug: function () {
         history.back();
       },
