0582e9906d68d7adb75d7cd214a46d3c0ac64df8
Robbe Van Duysen <robbe.vanduysen@student.hogent.be>
2024-05-20 22:40:35 +0200

    Fix: aanpassen gebruiker ww

--- P2_BSP_G1/app/p2_bsp_g1/webapp/controller/Profiel.controller.js
+++ P2_BSP_G1/app/p2_bsp_g1/webapp/controller/Profiel.controller.js
@@ -1,181 +1,215 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller", "sap/m/MessageBox", "sap/ui/model/json/JSONModel"],
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+  ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
-   */ 
-  function(Controller, MessageBox, JSONModel) {
-  "use strict";
+   */
+  function (Controller, MessageBox, JSONModel) {
+    "use strict";
 
-  var loggedInUser = JSON.parse(localStorage.getItem("user"))
+    var loggedInUser = JSON.parse(localStorage.getItem("user"));
 
-  return Controller.extend("p2bspg1.controller.Profiel", {
-    onInit: function() {
-      this.oOwnerComponent = this.getOwnerComponent();
-      this.oRouter = this.oOwnerComponent.getRouter();
-      this.oRouter
-        .getRoute("Profiel")
-        .attachPatternMatched(this._onRouteMatched, this);
-      console.log("gelukt");
+    return Controller.extend("p2bspg1.controller.Profiel", {
+      onInit: function () {
+        this.oOwnerComponent = this.getOwnerComponent();
+        this.oRouter = this.oOwnerComponent.getRouter();
+        this.oRouter
+          .getRoute("Profiel")
+          .attachPatternMatched(this._onRouteMatched, this);
+        console.log("gelukt");
 
-      var oRegistreer = {
-        voornaam: "",
-        achternaam: "",
-        email: "",
-        geboortedatum: null,
-        wachtwoord: "",
-        herhaalWachtwoord: ""
-      };
-    
-      var oModel = new JSONModel(oRegistreer);
-      this.getView().setModel(oModel, "form");
-    },
+        var oRegistreer = {
+          voornaam: "",
+          achternaam: "",
+          email: "",
+          geboortedatum: null,
+          wachtwoord: "",
+          herhaalWachtwoord: "",
+        };
 
-    updateModel: function() {
-      var oModel = this.getView().getModel("form");
-      var oFormData = oModel.getData();
-      oFormData.voornaam = this.getView().byId("voornaam").getValue();
-      oFormData.achternaam = this.getView().byId("achternaam").getValue();
-      oFormData.email = this.getView().byId("email").getValue();
-      oFormData.geboortedatum = this.getView().byId("geboortedatum").getValue();
-      oFormData.wachtwoord = this.getView().byId("wachtwoord").getValue();
-      oFormData.herhaalWachtwoord = this.getView().byId("herhaalWW").getValue();
-      oModel.setData(oFormData);
-    },
+        var oModel = new JSONModel(oRegistreer);
+        this.getView().setModel(oModel, "form");
+      },
 
-    _onRouteMatched: function(oEvent) {
-      var oView = this.getView();
-      console.log("logged in user: ", loggedInUser.gebruikerID);
-      var urlPath = "/" + "Gebruikers(gebruikerID=" + loggedInUser.gebruikerID + ")";
-      oView.bindElement({ path: urlPath });
-    },
+      updateModel: function () {
+        var oModel = this.getView().getModel("form");
+        var oFormData = oModel.getData();
+        oFormData.voornaam = this.getView().byId("voornaam").getValue();
+        oFormData.achternaam = this.getView().byId("achternaam").getValue();
+        oFormData.email = this.getView().byId("email").getValue();
+        oFormData.geboortedatum = this.getView()
+          .byId("geboortedatum")
+          .getValue();
+        oFormData.wachtwoord = this.getView().byId("wachtwoord").getValue();
+        oFormData.herhaalWachtwoord = this.getView()
+          .byId("herhaalWW")
+          .getValue();
+        oModel.setData(oFormData);
+      },
 
-    onTerug: function() {
-      history.back();
-    },
+      _onRouteMatched: function (oEvent) {
+        var oView = this.getView();
+        console.log("logged in user: ", loggedInUser.gebruikerID);
+        var urlPath =
+          "/" + "Gebruikers(gebruikerID=" + loggedInUser.gebruikerID + ")";
+        oView.bindElement({ path: urlPath });
+      },
 
-    validateForm: function(formData) {
-      for (var key in formData) {
-        if (formData.hasOwnProperty(key)) {
-          if (!formData[key] && key !== "herhaalWachtwoord") {
-            return false;
+      onTerug: function () {
+        history.back();
+      },
+
+      validateForm: function (formData) {
+        for (var key in formData) {
+          if (formData.hasOwnProperty(key)) {
+            if (!formData[key] && key !== "herhaalWachtwoord") {
+              return false;
+            }
           }
         }
-      }
-      return true;
-    },
-    
-    onOpslaan: function() {
-      this.updateModel();
-      var oForm = this.getView().getModel("form").getData();
-      console.log(oForm);
-      var sHerhaalWachtwoord = oForm.herhaalWachtwoord;
+        return true;
+      },
 
-      // Check if any field is empty
-      if (!this.validateForm(oForm)) {
-        MessageBox.error("Please fill in all fields");
-        return;
-      }
+      onOpslaan: function () {
+        this.updateModel();
+        var oForm = this.getView().getModel("form").getData();
+        console.log(oForm);
+        var sHerhaalWachtwoord = oForm.herhaalWachtwoord;
 
-      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(oForm.email)) {
-        MessageBox.error("Please enter a valid email address. Example: jan@example.com");
-        return;
-      }
+        // Check if any field is empty
+        if (!this.validateForm(oForm)) {
+          MessageBox.error("Please fill in all fields");
+          return;
+        }
 
-      // Check if password and repeat password match
-      if (oForm.wachtwoord !== sHerhaalWachtwoord) {
-        MessageBox.error("Passwords do not match! Please try again.");
-        return;
-      }
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(oForm.email)) {
+          MessageBox.error(
+            "Please enter a valid email address. Example: jan@example.com"
+          );
+          return;
+        }
 
-      var birthdate = new Date(oForm.geboortedatum);
-      var currentDate = new Date();
-      if (birthdate > currentDate) {
+        // Check if password and repeat password match
+        if (oForm.wachtwoord !== sHerhaalWachtwoord) {
+          MessageBox.error("Passwords do not match! Please try again.");
+          return;
+        }
+
+        var birthdate = new Date(oForm.geboortedatum);
+        var currentDate = new Date();
+        if (birthdate > currentDate) {
           MessageBox.error("Please enter a valid birthdate.");
           return;
-      }
+        }
 
-      MessageBox.confirm("Wilt u uw gegevens wijzigen?", {
-        actions: [MessageBox.Action.YES, MessageBox.Action.NO],
-        onClose: function(sAction) {
-          if (sAction === MessageBox.Action.YES) {
-            MessageBox.success("Uw gegevens zijn gewijzigd.");
-            this.getView().byId("wachtwoord").setValue("");
-            this.getView().byId("herhaalWW").setValue("");
-          } else {
-            MessageBox.error("Uw gegevens zijn niet gewijzigd.");
-            this.getView().byId("wachtwoord").setValue("");
-            this.getView().byId("herhaalWW").setValue("");
-          }
-        }.bind(this),
-      });
+        MessageBox.confirm("Wilt u uw gegevens wijzigen?", {
+          actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+          onClose: function (sAction) {
+            if (sAction === MessageBox.Action.YES) {
+              var odatamodel = this.getView().getModel("v2model"),
+                geboortedatum = new Date(oForm.geboortedatum),
+                oFormUpdate = {
+                  voornaam: oForm.voornaam,
+                  achternaam: oForm.achternaam,
+                  email: oForm.email,
+                  geboortedatum: geboortedatum,
+                  wachtwoord: oForm.wachtwoord,
+                },
+                that = this;
 
-    },
-    onEditVoornaam: function() {
-      var oInputVoornaam = this.getView().byId("voornaam");
-      var bEditable = oInputVoornaam.getEditable();
-      oInputVoornaam.setEditable(!bEditable);
-    },
-    onEditAchternaam: function() {
-      var oInputAchternaam = this.getView().byId("achternaam");
-      var bEditable = oInputAchternaam.getEditable();
-      oInputAchternaam.setEditable(!bEditable);
-    },
-    onEditEmail: function() {
-      var oInputEmail = this.getView().byId("email");
-      var bEditable = oInputEmail.getEditable();
-      oInputEmail.setEditable(!bEditable);
-    },
-    onEditDate: function() {
-      var oInputGeboortedatum = this.getView().byId("geboortedatum");
-      var bEditable = oInputGeboortedatum.getEditable();
-      oInputGeboortedatum.setEditable(!bEditable);
-    },
-    onEditWW: function() {
-      var oInputWachtwoord = this.getView().byId("wachtwoord");
-      var bEditable = oInputWachtwoord.getEditable();
-      oInputWachtwoord.setEditable(!bEditable);
-    },
-    onEditHerhaalWW: function() {
-      var oInputHerhaalWachtwoord = this.getView().byId("herhaalWW");
-      var bEditable = oInputHerhaalWachtwoord.getEditable();
-      oInputHerhaalWachtwoord.setEditable(!bEditable);
-    },
+              odatamodel.update(
+                "/Gebruikers(" + loggedInUser.gebruikerID + ")",
+                oFormUpdate,
+                {
+                  success: function () {
+                    MessageBox.success("Uw gegevens zijn gewijzigd.");
+                    that.getView().byId("wachtwoord").setValue("");
+                    that.getView().byId("herhaalWW").setValue("");
+                  },
+                  error: function () {
+                    MessageBox.error(
+                      "Er is iets misgegaan. Probeer het later opnieuw."
+                    );
+                  },
+                }
+              );
+            } else {
+              MessageBox.error("Uw gegevens zijn niet gewijzigd.");
+              this.getView().byId("wachtwoord").setValue("");
+              this.getView().byId("herhaalWW").setValue("");
+            }
+          }.bind(this),
+        });
+      },
+      onEditVoornaam: function () {
+        var oInputVoornaam = this.getView().byId("voornaam");
+        var bEditable = oInputVoornaam.getEditable();
+        oInputVoornaam.setEditable(!bEditable);
+      },
+      onEditAchternaam: function () {
+        var oInputAchternaam = this.getView().byId("achternaam");
+        var bEditable = oInputAchternaam.getEditable();
+        oInputAchternaam.setEditable(!bEditable);
+      },
+      onEditEmail: function () {
+        var oInputEmail = this.getView().byId("email");
+        var bEditable = oInputEmail.getEditable();
+        oInputEmail.setEditable(!bEditable);
+      },
+      onEditDate: function () {
+        var oInputGeboortedatum = this.getView().byId("geboortedatum");
+        var bEditable = oInputGeboortedatum.getEditable();
+        oInputGeboortedatum.setEditable(!bEditable);
+      },
+      onEditWW: function () {
+        var oInputWachtwoord = this.getView().byId("wachtwoord");
+        var bEditable = oInputWachtwoord.getEditable();
+        oInputWachtwoord.setEditable(!bEditable);
+      },
+      onEditHerhaalWW: function () {
+        var oInputHerhaalWachtwoord = this.getView().byId("herhaalWW");
+        var bEditable = oInputHerhaalWachtwoord.getEditable();
+        oInputHerhaalWachtwoord.setEditable(!bEditable);
+      },
 
-    onSeePassword: function() {
-      var oPasswordInput = this.getView().byId("wachtwoord");
-      var sCurrentType = oPasswordInput.getType();
-      if (sCurrentType === "Password") {
+      onSeePassword: function () {
+        var oPasswordInput = this.getView().byId("wachtwoord");
+        var sCurrentType = oPasswordInput.getType();
+        if (sCurrentType === "Password") {
           oPasswordInput.setType("Text");
-      } else {
+        } else {
           oPasswordInput.setType("Password");
-      }
-      var oCurrentIcon = this.getView().byId("icon");
-      console.log(oCurrentIcon);
-      var sCurrentIcon = oCurrentIcon.getIcon();
-      if (sCurrentIcon === "sap-icon://show") {
+        }
+        var oCurrentIcon = this.getView().byId("icon");
+        console.log(oCurrentIcon);
+        var sCurrentIcon = oCurrentIcon.getIcon();
+        if (sCurrentIcon === "sap-icon://show") {
           oCurrentIcon.setIcon("sap-icon://hide");
-      } else {
+        } else {
           oCurrentIcon.setIcon("sap-icon://show");
-      }
-    },
-    onSeePasswordHerhaal: function() {
-      var oPasswordInput = this.getView().byId("herhaalWW");
-      var sCurrentType = oPasswordInput.getType();
-      if (sCurrentType === "Password") {
+        }
+      },
+      onSeePasswordHerhaal: function () {
+        var oPasswordInput = this.getView().byId("herhaalWW");
+        var sCurrentType = oPasswordInput.getType();
+        if (sCurrentType === "Password") {
           oPasswordInput.setType("Text");
-      } else {
+        } else {
           oPasswordInput.setType("Password");
-      }
-      var oCurrentIcon = this.getView().byId("icon2");
-      console.log(oCurrentIcon);
-      var sCurrentIcon = oCurrentIcon.getIcon();
-      if (sCurrentIcon === "sap-icon://show") {
+        }
+        var oCurrentIcon = this.getView().byId("icon2");
+        console.log(oCurrentIcon);
+        var sCurrentIcon = oCurrentIcon.getIcon();
+        if (sCurrentIcon === "sap-icon://show") {
           oCurrentIcon.setIcon("sap-icon://hide");
-      } else {
+        } else {
           oCurrentIcon.setIcon("sap-icon://show");
-      }
-    },
-
-  });
-}); 
+        }
+      },
+    });
+  }
+);
